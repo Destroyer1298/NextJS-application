f0b999fab714b9ee9d8b462dfdd99660ba1abfc4
Your Name <you@example.com>
2023-04-02 15:30:27 -0400

    commit

--- pages/login.js
+++ pages/login.js
@@ -1,60 +1,58 @@
-import { Card, Form, Button, Alert } from "react-bootstrap";
+import { Card, Form, Alert, Button } from "react-bootstrap";
 import { useState } from 'react';
-import { authenticateUser } from '@/lib/authenticate';
+import { authenticateUser } from '@/lib/authenticate'
 import { useRouter } from 'next/router';
-import { useAtom } from 'jotai';
-import { searchHistoryAtom } from '@/store';
-import { favouritesAtom } from '@/store';
+import { favouritesAtom, searchHistoryAtom } from "@/store";
 import { getFavourites, getHistory } from "@/lib/userData";
-
-
+import { useAtom } from 'jotai'
 
 export default function Login(props) {
-	const router = useRouter();
-	const [user, setUser] = useState("");
-	const [password, setPassword] = useState("");
-	const [warning, setWarning] = useState("");
-	const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
-	const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [warning, setWarning] = useState('');
+    const router = useRouter();
+    const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
 
-	async function updateAtoms() {
-		setFavouritesList(await getFavourites());
-		setSearchHistory(await getHistory());
-	}
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            await authenticateUser(user, password);
+            await updateAtoms();
+            router.push('/favourites');
+        } catch (err) {
+            setWarning(err.message);
+        }
+    }
 
-	async function handleSubmit(e) {
-		e.preventDefault();
-		try {
-			await authenticateUser(user, password);
-			await updateAtoms();
-			router.push('/favourites');
-		} catch (err) {
-			setWarning(err.message);
-		}
-	}
+    async function updateAtoms() {
+        setFavouritesList(await getFavourites());
+        setSearchHistory(await getHistory());
+    }
 
-	return (
-		<>
-			<Card bg="light">
-				<Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
-			</Card>
-			<br />
-			<Form onSubmit={handleSubmit}>
-				<Form.Group>
-					<Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
-				</Form.Group>
-				<br />
-				<Form.Group>
-					<Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
-				</Form.Group>
-				<br />
-				<Form.Group>
-					{warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
-				</Form.Group>
-
-				<br />
-				<Button variant="primary" className="pull-right" type="submit">Login</Button>
-			</Form>
-		</>
-	);
+    return (
+        <>
+            <Card bg="light">
+                <Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
+            </Card>
+            <br />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
+                </Form.Group>
+                <br />
+                <Form.Group>
+                    <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <br />
+                {warning && (
+                    <>
+                        <br />
+                        <Alert variant="danger">{warning}</Alert>
+                    </>
+                )}
+                <Button variant="primary" className="pull-right" type="submit">Login</Button>
+            </Form>
+        </>
+    );
 }
